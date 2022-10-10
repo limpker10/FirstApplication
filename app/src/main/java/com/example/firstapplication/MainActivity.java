4a18fc2985c5801b480b3acff799d91c087d5c7b
jose miguuel cano vilcapaza <jcanov@unsa.edu.pe>
2022-10-10 17:33:15 -0500

    Completo, Lab01 problema resuelto

--- app/src/main/java/com/example/firstapplication/MainActivity.java
+++ app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -4,19 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText edtUsuario = findViewById(R.id.edtUsuario);
+        EditText edtPassword = findViewById(R.id.edtPassword);
+        TextView txtMensaje = findViewById(R.id.txtMensaje);
+        Button btnLogin = (Button) findViewById(R.id.btnLogin);
 
-        //Clase estatica log, para imprimir en consola
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usuario = edtUsuario.getText().toString();
+                String password = edtPassword.getText().toString();
+                if (usuario.equals("test") && password.equals("1234")) {
+                    Log.d(TAG,"valida," + usuario + "," + password);
+                    txtMensaje.setText("Autenticacion valida");
+                } else {
+                    txtMensaje.setText("Autenticacion no valida");
+                    Log.d(TAG,"invalido," + usuario + "," + password);
+                }
+            }
+        });
+
+        /**
+        Clase estatica log, para imprimir en consola
         Log.i("info","Informacion"); //info
         Log.d("Debug","valor Debug"); // debug
         Log.w("warning","Valor Warning");
         Log.e("Error","Valor Error");
         Log.v("Verbose","Valor Verbose");
+
+         */
     }
 }
