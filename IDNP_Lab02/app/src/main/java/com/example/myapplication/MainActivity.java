3875a1f42701be644e295361cb125c2203c50de9
jose miguuel cano vilcapaza <jcanov@unsa.edu.pe>
2022-10-29 07:49:57 -0500

    Laboratorio02, completado

--- IDNP_Lab02/app/src/main/java/com/example/myapplication/MainActivity.java
+++ IDNP_Lab02/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,102 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+    private static String TAG = "MainActivity";
+
+    private void resetForm(EditText form[], TextView txtMensaje) throws InterruptedException {
+        TimeUnit.SECONDS.sleep(1);
+
+        for (int i = 0; i < form.length; i++) {
+            form[i].setText("");
+        }
+        txtMensaje.setText("");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<Postulante> registro = new ArrayList<Postulante>();
+        EditText form[] = {
+                findViewById(R.id.editNombres),
+                findViewById(R.id.editPaterno),
+                findViewById(R.id.editMaterno),
+                findViewById(R.id.editFecNacimiento),
+                findViewById(R.id.editColegio),
+                findViewById(R.id.editCarrera),
+        };
+
+
+        TextView txtMensaje = findViewById(R.id.txtMensaje);
+        Button btnRegistrar = (Button) findViewById(R.id.btnRegistro);
+        Button btnVerLista = (Button) findViewById(R.id.btnListaPostulantes);
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtMensaje.setText("Registro  Exitoso");
+                registro.add(Postulante.createPostulante(form));
+                try {
+                    resetForm(form, txtMensaje);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        btnVerLista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                for (int i = 0; i < registro.size(); i++) {
+
+                    Log.d(TAG, "valida," + registro.get(i));
+                }
+            }
+        });
+
+
+        /**
+         * EditText edtNombres = findViewById(R.id.editNombres);
+         *         EditText edtPaterno = findViewById(R.id.editPaterno);
+         *         EditText edtMaterno = findViewById(R.id.editMaterno);
+         *         EditText edtFecNaci = findViewById(R.id.editFecNacimiento);
+         *         EditText edtColegio = findViewById(R.id.editColegio);
+         *         EditText edtCarrera = findViewById(R.id.editCarrera);
+         *         TextView txtMensaje = findViewById(R.id.txtMensaje);
+         EditText edtUsuario = findViewById(R.id.edtUsuario);
+         EditText edtPassword = findViewById(R.id.edtPassword);
+         TextView txtMensaje = findViewById(R.id.txtMensaje);
+         Button btnLogin = (Button) findViewById(R.id.btnLogin);
+         btnLogin.setOnClickListener(new View.OnClickListener() {
+        @Override public void onClick(View view) {
+        String usuario = edtUsuario.getText().toString();
+        String password = edtPassword.getText().toString();
+        if (usuario.equals("test") && password.equals("1234")) {
+        Log.d(TAG,"valida," + usuario + "," + password);
+        txtMensaje.setText("Autenticacion valida");
+        } else {
+        txtMensaje.setText("Autenticacion no valida");
+        Log.d(TAG,"invalido," + usuario + "," + password);
+        }
+        }
+        });
+         Clase estatica log, para imprimir en consola
+         Log.i("info","Informacion"); //info
+         Log.d("Debug","valor Debug"); // debug
+         Log.w("warning","Valor Warning");
+         Log.e("Error","Valor Error");
+         Log.v("Verbose","Valor Verbose");
+         */
     }
 }
---
+++ IDNP_Lab02/app/src/main/java/com/example/myapplication/Postulante.java
@@ -0,0 +1,46 @@
+package com.example.myapplication;
+
+import android.widget.EditText;
+
+public class Postulante {
+
+    private String edtNombres;
+    private String edtPaterno;
+    private String edtMaterno;
+    private String edtFecNacimiento;
+    private String edtColegio;
+    private String edtCarrera;
+
+    public Postulante(String edtNombres, String edtPaterno, String edtMaterno, String edtFecNacimiento, String edtColegio, String edtCarrera) {
+        this.edtNombres = edtNombres;
+        this.edtPaterno = edtPaterno;
+        this.edtMaterno = edtMaterno;
+        this.edtFecNacimiento = edtFecNacimiento;
+        this.edtColegio = edtColegio;
+        this.edtCarrera = edtCarrera;
+    }
+
+    public static Postulante createPostulante(EditText form [] ){
+
+        return new Postulante(
+                form[0].getText().toString(),
+                form[1].getText().toString(),
+                form[2].getText().toString(),
+                form[3].getText().toString(),
+                form[4].getText().toString(),
+                form[5].getText().toString()
+        );
+
+    }
+    @Override
+    public String toString() {
+        return "Postulante{" +
+                "edtNombres='" + edtNombres + '\n' +
+                ", edtPaterno='" + edtPaterno + '\n' +
+                ", edtMaterno='" + edtMaterno + '\n' +
+                ", edtFecNacimiento='" + edtFecNacimiento + '\n' +
+                ", edtColegio='" + edtColegio + '\n' +
+                ", edtCarrera='" + edtCarrera + '\n' +
+                '}';
+    }
+}
