7787e5ef85242415c761b070e0f157e45827ef73
Luis Condori <lcondorivill@unsa.edu.pe>
2022-11-21 22:57:13 -0500

    Grafico circular terminado

--- IDNP_Lab06/app/src/main/java/com/example/idnp_lab06/GraficoCircular.java
+++ IDNP_Lab06/app/src/main/java/com/example/idnp_lab06/GraficoCircular.java
@@ -4,11 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 public class GraficoCircular extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grafico_circular);
+
+        Button btnGraficoCircular;
+        btnGraficoCircular = (Button) findViewById(R.id.buttonAtrasGraficoCircular);
+
+        btnGraficoCircular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
